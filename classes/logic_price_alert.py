3bdf25bcf26627593aa68d7261490fbbff452197
Administrator <Administrator@WIN-MEE7SG0T5JC>
2023-04-20 19:47:24 +0800

    change source to yahoo screener

--- classes/logic_price_alert.py
+++ classes/logic_price_alert.py
@@ -2,8 +2,9 @@ import logging
 import telegram
 import gspread
 import httpx
+import asyncio
+import json
 import pandas as pd
-import pandas_datareader as pdr
 from decimal import Decimal
 import time
 
@@ -22,20 +23,78 @@ class PriceAlertLogic:
         else:
             await self.send(f'\U00002757 ERROR: {self.config["google_spreadsheet"]["file_name"]} spreadsheet does not have header columns')
  
+    async def create_query_task(self, client, symbols, quote_type):
+        url = "https://query1.finance.yahoo.com/v1/finance/screener?crumb=LKFH6LnWCm2&lang=en-US&region=US&formatted=true&corsDomain=finance.yahoo.com"
+        header = {
+            "cookie": "B=6upmke5hvjjuh&b=3&s=59; A1=d=AQABBNHP-WMCEKMQh8VhePnVCOUHhXHUZm8FEgEBCAF-P2RrZFpOb2UB_eMBAAcI0c_5Y3HUZm8&S=AQAAAg_MngjDSx_G5gJ6ncC4l7o; A3=d=AQABBNHP-WMCEKMQh8VhePnVCOUHhXHUZm8FEgEBCAF-P2RrZFpOb2UB_eMBAAcI0c_5Y3HUZm8&S=AQAAAg_MngjDSx_G5gJ6ncC4l7o; GUC=AQEBCAFkP35ka0IirQUd; A1S=d=AQABBNHP-WMCEKMQh8VhePnVCOUHhXHUZm8FEgEBCAF-P2RrZFpOb2UB_eMBAAcI0c_5Y3HUZm8&S=AQAAAg_MngjDSx_G5gJ6ncC4l7o&j=WORLD; PRF=t%3DGC%253DF%252BZN%253DF%252BNGT%253DF%252BSGD%253DX%252BA%252BGOOG%252BGFQ23.CME%252B%255EJKSE%252BEC%26newChartbetateaser%3D1; cmp=t=1681981788&j=0&u=1---",
+            "content-type": "application/json"
+        }
+        payload = {
+            "size": 100,
+            "offset": 0,
+            "sortField": "ticker",
+            "sortType": "DESC",
+            "quoteType": quote_type,
+            "topOperator": "AND",
+            "query": {
+                "operator": "AND",
+                "operands": [
+                    {
+                        "operator": "or",
+                        "operands": []
+                    }
+                ]
+            },
+            "userId": "",
+            "userIdType": "guid"
+        }
+
+        for symbol in symbols:
+            payload["query"]["operands"][0]["operands"].append({
+                    "operator": "EQ",
+                    "operands": [
+                        "ticker",
+                        symbol
+                    ]
+                }) 
+
+        logging.info(f"Downloading {quote_type}")
+        response = await client.post(url=url, headers=header, data=json.dumps(payload))
+        if response is not None:
+            json_response = response.json()
+            return [{"symbol": a["symbol"], "price": a["regularMarketPrice"]["raw"]} for a in json_response["finance"]["result"][0]["quotes"]]
+
+        return None
+
     async def get_quote_with_failover(self, symbols):
+        result = pd.DataFrame()
         retry = 0
         while retry < 3:
             try:    
                 if retry > 0: 
                     time.sleep(1)
+                
+                async with httpx.AsyncClient() as client:
+                    tasks = [
+                        asyncio.ensure_future(self.create_query_task(client, symbols, "EQUITY")),
+                        asyncio.ensure_future(self.create_query_task(client, symbols, "FUTURE")),
+                        asyncio.ensure_future(self.create_query_task(client, symbols, "CURRENCY")),
+                        asyncio.ensure_future(self.create_query_task(client, symbols, "ETF"))
+                    ]
+                    
+                    gather_results = await asyncio.gather(*tasks)
 
-                result = pdr.get_quote_yahoo(symbols)
+                    for items in gather_results:
+                        if items is not None and len(items) > 0:
+                            result = pd.DataFrame(items) if result.empty else result._append(items)
+
                 return result
             except:
-                retry += 1
-        
-        await self.send(f"\U00002757 ERROR: 3 attempts have failed reading quotes {self.df.symbol.to_list()}")
-        return pd.DataFrame()
+                retry += 1                
+                if retry > 2:
+                    await self.send(f"\U00002757 ERROR: 3 attempts have failed reading quotes {self.df.symbol.to_list()}")
+
+        return result
 
     async def scan(self):
         try:
@@ -44,7 +103,7 @@ class PriceAlertLogic:
             if not quotes.empty:
                 new_sheet = [["symbol", "operator", "value"]]
                 for index, item in self.df.iterrows():
-                    is_triggered = await self.validate(quotes.loc[item.symbol], item)
+                    is_triggered = await self.validate(quotes[quotes.symbol == item.symbol].iloc[0], item)
                     if not is_triggered:
                         new_sheet.append(item.to_list())
 
@@ -55,6 +114,7 @@ class PriceAlertLogic:
             logging.exception(ex)
 
     async def validate(self, quote, item):
+        result = False
         try:
             if quote is not None:
                 quote_price = quote.price
@@ -63,18 +123,18 @@ class PriceAlertLogic:
                     case "gt":
                         if quote_price >= target_price:
                             await self.send(f"\U00002714 {item.symbol} is greater or equal than {target_price}")
-                            return True
+                            result = True
                     case "lt":
                         if quote_price <= target_price:
                             await self.send(f"\U00002714 {item.symbol} is less or equal than {target_price}")
-                            return True
+                            result = True
                     case _:
                         await self.send(f"\U00002757 ERROR: quote {item.symbol} has invalid operator {item.operator}")            
-                        return True
+                        result = True
         except Exception as ex: 
             await self.send(f"\U00002757 ERROR: validating quote {item.symbol}")
             logging.exception(ex)          
-        return False
+        return result
 
     def connect_google_spreadsheet(self):
         gc = gspread.service_account_from_dict(self.config["google_spreadsheet"]["credential"])
